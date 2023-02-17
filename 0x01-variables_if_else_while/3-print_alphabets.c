aba030ed3130a5cba8c56676029f9ba168b1bfc1
Lungastafa2 <lungastafa@gmail.com>
2023-02-17 15:15:26 -0800

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * Description: main - prints diffrent result as n is either =>< 5
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+
+	int x;
+
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	char small = tolower(x);
+
+	putchar(small);
+	}
+	 for (x = 'A'; x <= 'Z'; x++)
+        {
+        char upper = x;
+
+        putchar(upper);
+        }
+	putchar('\n');
+
+	return (0);
+}
