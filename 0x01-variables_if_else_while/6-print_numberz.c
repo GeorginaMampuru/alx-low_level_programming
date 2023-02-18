019eb2194aaf9d52c32ab13c61e7bfba05599adc
Lungastafa2 <lungastafa@gmail.com>
2023-02-17 16:38:55 -0800

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * Description: main - prints diffrent result as n is either =>< 5
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+
+	int x;
+
+	for (x = 0; x < 10; x++)
+	putchar( x);
+       	putchar('\n');
+       	return (0);
+}
