93f48e31a37fca66bc440ffe9d25d0282e865692
Lungastafa2 <lungastafa@gmail.com>
2023-02-17 17:25:35 -0800

    num4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,14 +4,15 @@
 #include <unistd.h>
 #include <ctype.h>
 /**
- * Description: main - prints diffrent result as n is either =>< 5
- * Return: 0 if success.
- */
+* Description: main - prints diffrent result as n is either =>< 5
+* Return: 0 if success.
+*/
 
 int main(void)
 {
 
 	char x, q, e;
+
 	e = 'e';
 	q = 'q';
 
