66a89f9db922f149367584323ac51ab2bb34bd05
Lungastafa2 <lungastafa@gmail.com>
2023-02-17 16:08:57 -0800

    com

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * Description: main - prints diffrent result as n is either =>< 5
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+
+	char x,q,e;
+
+	e = 'e';
+	q ='q';
+
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != q && x != e)
+		{	
+		putchar(x);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
