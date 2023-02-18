15a178821094fe91325141cb7ab3829d4c897587
Lungastafa2 <lungastafa@gmail.com>
2023-02-17 16:47:41 -0800

    lu

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <ctype.h>
 /**
- * Description: main - prints diffrent result as n is either =>< 5
- * Return: 0 if success.
- */
+* Description: main - prints diffrent result as n is either =>< 5
+* Return: 0 if success.
+*/
 
 int main(void)
 {
