653661f384bd9dee9e54b7d4cb7d6292a41bc387
Lungastafa2 <lungastafa@gmail.com>
2023-02-17 17:12:34 -0800

    yes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+* Description: main - prints diffrent result as n is either =>< 5
+* Return: 0 if success.
+*/
+
+int main(void)
+{
+
+	int x;
+	char z;
+
+	for (x = '0'; x <= '9'; x++)
+	putchar(x);
+
+	for (z = 'a'; z <= 'f'; z++)
+        putchar(z);
+	putchar('\n');
+	return (0);
+}
