ff9e3cc62e12aa27fd722af3178eac8bac53882e
Lungastafa2 <lungastafa@gmail.com>
2023-02-23 12:59:56 -0800

    numner 1 positive_negetive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+#include "stdlib.h"
+#include "main.h"
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
