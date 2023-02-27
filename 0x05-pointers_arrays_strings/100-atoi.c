3f11ce9ae64612e3856c82c77411a42dfffeb68c
Lungastafa2 <lungastafa@gmail.com>
2023-02-27 13:25:36 -0800

    advance and corrected Questions

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+* Description: main - prints diffrent result as n is either =>< 5
+* Return: 0 if success.
+*/
+
+int main(void)
+{
+
+	int x, z;
+
+	for (x = '0'; x < '9'; x++)
+	{
+	for (z = '0'; z <= '9'; z++)
+	{
+	if (x != z && x < z)
+	{
+	putchar(x);
+	putchar(z);
+	if (x == '8' && z == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
