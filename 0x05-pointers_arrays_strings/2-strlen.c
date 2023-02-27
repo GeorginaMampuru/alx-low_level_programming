042fd4321a48d62249df148981bdcea200023b6d
Lungastafa2 <lungastafa@gmail.com>
2023-02-27 13:09:18 -0800

    mandotory Questions

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: the string to get the length of s
+ * Return: the length of @s.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
+}
