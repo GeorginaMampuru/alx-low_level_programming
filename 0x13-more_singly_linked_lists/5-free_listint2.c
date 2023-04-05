a655f8a4e655f06a10b12cc2bcff32441151b63d
Lungastafa2 <lungastafa@gmail.com>
2023-04-05 04:03:49 -0700

    correct number 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *free_listint2 - frees a linked list
+ *free_listint2 - a function that frees a listint_t list
  *@head: pointer to the head of the list
  *
  *Return: void
