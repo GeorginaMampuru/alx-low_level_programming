382108b0d80e6e9126d2768bbb5ebca9774fe9ab
Lungastafa2 <lungastafa@gmail.com>
2023-04-05 03:55:28 -0700

    correcting 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
-include "lists.h"
+#include "lists.h"
 
 /**
- * free_listint - frees a linked list
+ * free_listint - function that frees a listint_t list
  * @head: head of the list
  *
  * Return: void
