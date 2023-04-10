355af32f9eeee65046be5a73c58c19bb9caece20
Lungastafa2 <lungastafa@gmail.com>
2023-04-10 10:31:09 -0700

    attept4

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,52 +1,33 @@
-#include <stddef.h>
 #include "main.h"
+#include <stddef.h>
 
 /**
- * check_valid_string - checks if a string is a valid binary
- * @b: string to be checked
- *
- * Return: 1 if string is valid and 0 if not
- */
-
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-
-	return (1);
-}
-
-
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
- *
- */
+  * binary_to_uint - converts a binary number to an unsigned int
+  * @b: the binary number
+  * Return: the unsigned int
+  */
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i = 0;
 	unsigned int decimal = 0;
-	int str_len = 0;
-	int base = 1;
 
-	if (!check_valid_string(b))
-		return (0);
-	while (b[str_len] != '\0')
-		str_len++;
-	while (str_len)
+	if (b == NULL)
 	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
+		return (0);
 	}
 
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		decimal = decimal * 2;
+		if (b[i] == '1')
+		{
+			decimal = decimal + 1;
+		}
+	}
 	return (decimal);
 }
